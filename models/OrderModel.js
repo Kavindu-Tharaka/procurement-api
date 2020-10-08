8d2253f303810e08c033f194da2fe7f1cc520d9a
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-10-08 14:17:51 +0530

    small changes

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -1,43 +1,51 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Supplier = require('./Supplier');
 
-const OrderSchema = mongoose.Schema({
-  siteName: {
-    type: String,
-  },
-  siteAddress: {
-    type: String,
-  },
-  orderItems: [
-    {
-      material: {
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    siteName: {
         type: String,
-        required: [true, 'Material is Mandatory!']
-      },
-      unit: {
-        type: String,
-        required: [true, 'Unit is Mandatory!']
-      },
-      quantity: {
-        type: Number,
-        required: [true, 'Quantity is Mandatory!']
-      },
-      remark: {
-        type: String,
-      },
-      status: {
-        type: String,
-        trim: true,
-      },
-      deliverDate: {
-        type: Date,
-        required: [true, 'Deliver Date is Mandatory!']
-      },
-      createdDate: {
-        type: Date,
-        default: Date.now,
-      },
     },
-  ],
+    siteAddress: {
+        type: String,
+    },
+    supplier: {
+        type: Schema.Types.ObjectId,
+        ref: Supplier,
+    },
+    orderItems: [
+        {
+            material: {
+                type: String,
+                required: [true, 'Material is Mandatory!'],
+            },
+            unit: {
+                type: String,
+                required: [true, 'Unit is Mandatory!'],
+            },
+            quantity: {
+                type: Number,
+                required: [true, 'Quantity is Mandatory!'],
+            },
+            remark: {
+                type: String,
+            },
+            status: {
+                type: String,
+                trim: true,
+            },
+            deliverDate: {
+                type: Date,
+                required: [true, 'Deliver Date is Mandatory!'],
+            },
+            createdDate: {
+                type: Date,
+                default: Date.now,
+            },
+        },
+    ],
 });
 
-module.exports = mongoose.model("order", OrderSchema);
+const Order = mongoose.model("order", OrderSchema);
+module.exports = Order;
--- models/Supplier.js
+++ models/Supplier.js
@@ -1,31 +1,22 @@
 const mongoose = require('mongoose');
 const Order = require('./OrderModel');
 
-const SupplierSchema = mongoose.Schema({
-    supplierItems: [
-        {
-            supplierName: {
-                type: String,
-                trim: true,
-                required: [true, 'Suppiler Name is Mandatory!'],
-            },
-            address: {
-                type: String,
-                required: [true, 'Address is Mandatory!'],
-            },
-            previousOrders: [
-                {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: Order,
-                    trim: true,
-                },
-            ],
-            addedDate: {
-                type: Date,
-                default: Date.now,
-            },
-        },
-    ],
+const Schema = mongoose.Schema;
+
+const SupplierSchema = new Schema({
+    supplierName: {
+        type: String,
+        trim: true,
+        required: [true, 'Suppiler Name is Mandatory!'],
+    },
+    address: {
+        type: String,
+        required: [true, 'Address is Mandatory!'],
+    },
+    addedDate: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model('supplier', SupplierSchema);
