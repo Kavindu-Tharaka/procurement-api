9b4d608e5aa02709a48f0b1c14711909f496bf08
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-10-08 11:33:57 +0530

    refine order backend

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,28 +1,96 @@
 const Order = require('../models/OrderModel');
 
-
-exports.getOrders = async(req,res) => {
+exports.createOrder = async (req, res) => {
     try {
-        const results = await Order.find();
-        res,send(results);
-        
-    } catch (error) {
-        res.send(error.message);
-        console.log(error.message)
-    }
-}
+        const newOrder = await Order.create(req.body);
 
-exports.addOrder = async (req,res) => {
-    
+        res.status(201).json({
+            status: 'success',
+            data: {
+                order: newOrder,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
+exports.getAllOrders = async (req, res) => {
     try {
-       const newOrder = new Order({
-           orderItems : req.body.orderItems
-       })
+        const query = Order.find(req.query);
 
-        const result =await newOrder.save()
-        res.send(result)
-    } catch (error) {
-        res.send(error.message)
-        console.log(error.message)
+        const orders = await query;
+
+        res.status(200).json({
+            status: 'success',
+            results: orders.length,
+            data: {
+                orders,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
     }
-}
+};
+
+exports.getOrder = async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                order,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
+exports.updateOrder = async (req, res) => {
+    try {
+        const order = await Order.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                order,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
+exports.deleteOrder = async (req, res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const orderController = require("../controllers/orderController");
+const express = require('express');
+const orderController = require('../controllers/orderController');
 const router = express.Router();
 
-router.route("/").get(orderController.getOrders);
-router.route("/addOrder").post(orderController.addOrder);
+router
+	.route('/')
+	.post(orderController.createOrder)
+	.get(orderController.getAllOrders);
 
+router
+	.route('/:id')
+	.get(orderController.getOrder)
+	.patch(orderController.updateOrder)
+	.delete(orderController.deleteOrder);
 
-module.exports = router
+module.exports = router;
