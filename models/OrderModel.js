1c35f341a159f53950689a22ed3af8765962b846
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-10-08 13:05:29 +0530

    order refined

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
+  siteName: {
+    type: String,
+  },
+  siteAddress: {
+    type: String,
+  },
   orderItems: [
     {
       material: {
