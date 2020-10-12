9d7911c6f5c2a231882f2682c6168633ebdf4b41
chamodSamajith <chamodsr@gmail.com>
2020-10-12 20:35:19 +0530

    store keeper controller added

---
+++ controllers/storeKeeperController.js
@@ -0,0 +1,96 @@
+const Material = require('../models/StoreKeeper');
+
+exports.createOrder = async (req, res) => {
+    try {
+        const newMaterial = await Material.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                order: newMaterial,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
+exports.getAllMaterials = async (req, res) => {
+    try {
+        const query = Material.find(req.query); //.populate("supplier");
+
+        const materials = await query;
+
+        res.status(200).json({
+            status: 'success',
+            results: materials.length,
+            data: {
+                materials,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
+exports.getMaterial = async (req, res) => {
+    try {
+        const material = await Material.findById(req.params.id);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                material,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
+exports.updateMaterial  = async (req, res) => {
+    try {
+        const material = await Material.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                material,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
+exports.deleteMaterial = async (req, res) => {
+    try {
+        await Material.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failed',
+            message: err.message,
+        });
+    }
+};
+
